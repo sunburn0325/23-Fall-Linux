fccaf337e370720d148b2af4c18e37d61205a0b7
sunburn0325 <yjh3702@naver.com>
2023-10-06 15:24:16 +0900

    file added first

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "copy.h"
+
+#define MAXLINE 100
+char line[MAXLINE]; // 입력 줄
+char longest[MAXLINE]; // 가장 긴 줄
+
+
+int main()
+{
+	int len;
+	int max;
+	max = 0;
+	while (fgets(line, MAXLINE, stdin) != NULL) {
+		len = strlen(line);
+		if (len > max) {
+			max = len;
+			copy(line, longest);
+		}
+	}
+	if (max > 0) // 입력 줄이 있었다면
+		printf("%s", longest);
+	return 0;
+}
